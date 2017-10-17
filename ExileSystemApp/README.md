df9c255342c9a3a947785f53f8818cf9f74dfe63
Viktor Gullmark <viktor.gullmark@exor.se>
2017-10-17 15:29:52 +0200

    Initial commit for client

---
+++ ExileSystemApp/README.md
@@ -0,0 +1,11 @@
+# Introduction
+
+App for Exile-Systems built in angular-electron
+
+Currently runs with:
+
+- Angular v4.3.5
+- Angular-CLI v1.4.1
+- Electron v1.7.6
+- Electron Packager v9.0.1
+
---
+++ ExileSystemApp/src/app/components/home/home.component.ts
@@ -0,0 +1,21 @@
+import { ElectronService } from '../../providers/electron.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  constructor(private electron: ElectronService) { }
+
+  ngOnInit() {
+
+    this.electron.lineReader.eachLine('C:/Users/vgk/documents/test.txt', function (line, last) {
+      console.log(line);
+
+      return false;
+    });
+  }
+
+}
