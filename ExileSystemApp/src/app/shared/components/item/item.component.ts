639bfed1adb06706e992d0fd9f736812eddf485b
Viktor Gullmark <viktor.gullmark@exor.se>
2017-10-23 15:39:26 +0200

    Reworked character-preview to work with modals instead

--- ExileSystemApp/src/app/shared/components/item/item.component.ts
+++ ExileSystemApp/src/app/shared/components/item/item.component.ts
@@ -1,5 +1,5 @@
 import { Item } from '../../interfaces/item.interface';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-item',
@@ -10,8 +10,37 @@ export class ItemComponent implements OnInit {
   @Input() item: Item;
   @Input() columnsWide: number;
   @Input() columnsHigh: number;
+
   showTooltip = false;
   constructor() { }
   ngOnInit() {
+  }
+  getExplicitModClass(explicit) {
+    let modClass = '';
+
+    if (explicit.indexOf('to maximum Life') >= 0) {
+      modClass = 'life';
+    }
+    if (explicit.indexOf('to Cold Resistance') >= 0) {
+      modClass = 'coldRes';
+    }
+    if (explicit.indexOf('to Fire Resistance') >= 0) {
+      modClass = 'fireRes';
+    }
+    if (explicit.indexOf('to Lightning Resistance') >= 0) {
+      modClass = 'lightningRes';
+    }
+    if (explicit.indexOf('to Chaos Resistance') >= 0) {
+      modClass = 'chaosRes';
+    }
+    if (explicit.indexOf('to maximum Mana') >= 0) {
+      modClass = 'mana';
+    }
+
+    return modClass;
+  }
+
+  hideTooltip() {
+
   }
 }
