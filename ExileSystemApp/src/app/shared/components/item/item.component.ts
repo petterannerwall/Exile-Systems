84d80be7b48bdd80a7a9cdd0ba1d2d778b3bbd2a
Viktor Felix Gullmark <viktor.gullmark@hotmail.com>
2017-10-23 02:06:27 +0200

    Improved character-preview

--- ExileSystemApp/src/app/shared/components/item/item.component.ts
+++ ExileSystemApp/src/app/shared/components/item/item.component.ts
@@ -8,6 +8,8 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
   @Input() item: Item;
+  @Input() columnsWide: number;
+  @Input() columnsHigh: number;
   showTooltip = false;
   constructor() { }
   ngOnInit() {
