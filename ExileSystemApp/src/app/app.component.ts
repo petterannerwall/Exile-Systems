b967294540459fae869c9a9a3def94119c2802cc
Petter <petterannerwall@gmail.com>
2017-10-24 09:22:54 +0200

    Updates

--- ExileSystemApp/src/app/app.component.ts
+++ ExileSystemApp/src/app/app.component.ts
@@ -3,6 +3,8 @@ import { SettingService } from './shared/providers/setting.service';
 import { SignalRService } from './shared/providers/signalr.service';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ElectronService } from './shared/providers/electron.service';
+import { CurrencyService } from './shared/providers/currency.service';
+
 
 @Component({
   selector: 'app-root',
@@ -12,7 +14,7 @@ import { ElectronService } from './shared/providers/electron.service';
 
 export class AppComponent implements OnInit {
   constructor(public electronService: ElectronService, public settingService: SettingService, public signalRService: SignalRService,
-  private externalService: ExternalService) {
+    private externalService: ExternalService, private currencyService: CurrencyService) {
     if (electronService.isElectron()) {
       console.log('Mode electron');
       // Check if electron is correctly injected (see externals in webpack.config.js)
@@ -27,15 +29,15 @@ export class AppComponent implements OnInit {
 
     if (!fs.existsSync('./logout.exe')) {
       const file = electronService.fs.createWriteStream('./logout.exe');
-      const request = this.electronService.http.get('http://www.petterannerwall.se/logout.exe', function(response) {
+      const request = this.electronService.http.get('http://www.petterannerwall.se/logout.exe', function (response) {
         response.pipe(file);
-        file.on('finish', function() {
+        file.on('finish', function () {
           file.close();  // close() is async, call cb after close completes.
         });
-      }).on('error', function(err) { // Handle errors
+      }).on('error', function (err) { // Handle errors
         fs.unlink('./logout.exe'); // Delete the file async. (But we don't check the result)
       });
-  }
+    }
 
 
   }
