91acb29fa6a3025a4c506a40db0d067626449b93
Petter Annerwall <petterannerwall@gmail.com>
2017-11-20 13:11:51 +0100

    Added pricecheck service

---
+++ ExileSystemApp/src/app/shared/providers/pricesheck.service.ts
@@ -0,0 +1,16 @@
+
+import 'rxjs/add/operator/map';
+
+import { Injectable } from '@angular/core';
+import { RobotService } from './robot.service';
+import { ElectronService } from './electron.service';
+
+
+@Injectable()
+export class PricecheckService {
+
+
+  constructor() {
+
+  }
+}
